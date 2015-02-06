1b51518615f1b74b1e8f3e02a49a8263d1a7e327
dibsyjr1 <dibsy-jr@hotmail.co.uk>
2015-02-06 15:43:33 +0000

    test

--- app/src/main/java/com/alasdairdibben/meetem/MenuActivity.java
+++ app/src/main/java/com/alasdairdibben/meetem/MenuActivity.java
@@ -2,6 +2,10 @@ package com.alasdairdibben.meetem;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,4 +21,15 @@ public class MenuActivity extends ActionBarActivity {
      protected void onResume() {
          super.onResume();
      }
+
+     Button button = (Button) findViewById(R.id.map);
+     button.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             Intent i = new Intent(MenuActivity.this, MapsActivity.class);
+             startActivity(i);
+         }
+     });
+
+
 }
